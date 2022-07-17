e982e2a44c6017fd032cc68d9d880b35f2194026
pdukaf <pdukaf@outlook.com>
2022-07-18 00:29:59 +0200

    Defined namespace and routes for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :reservations
-
+  namespace :api do
+    namespace :v1 do
+      resources :reservations
+    end
+  end
   root 'reservations#index'
 end
