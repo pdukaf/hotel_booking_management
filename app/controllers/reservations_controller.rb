650c6d8ebb48af23012f0dab88261bee2e702566
pdukaf <pdukaf@outlook.com>
2022-07-17 17:57:59 +0200

    Added logic to create reservation and added template for the reservation show action

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,11 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservation, only: %i[ show edit update destroy ]
+
   def index
     @reservations = Reservation.all
+  end
+
+  def show
   end
 
   def new
@@ -8,10 +13,17 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+
+    respond_to do |format|
+      if @reservation.save
+        format.html { redirect_to reservation_url(@reservation), notice: "Reservation was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def show
-  end
 
   def edit
   end
@@ -20,5 +32,15 @@ class ReservationsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:hotel_name, :price, :currency, :arrival_date, :departure_date, :guest_name, :guest_email)
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
   end
 end
