dae95c85b2a6119a2c881b2b07bde105bb39103a
pdukaf <pdukaf@outlook.com>
2022-07-18 00:39:48 +0200

    Added API to fetch the specific reservation.

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,11 +1,21 @@
 module Api
   module V1
     class ReservationsController < ApiController
-
+      before_action :reservation, only: %i[ show ]
       def index
         reservations = Reservation.all
 
         render locals: { reservations: reservations }
+      end
+
+      def show
+        render :show, locals: { reservation: reservation }
+      end
+
+      private
+
+      def reservation
+        @reservation = Reservation.find(params[:id])
       end
     end
   end
---
+++ app/views/api/v1/reservations/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! reservation, :id, :hotel_name, :price, :currency, :arrival_date, :departure_date, :guest_name, :guest_email
