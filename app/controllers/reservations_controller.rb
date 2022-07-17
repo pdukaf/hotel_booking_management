f8c859af5eeab2f99f083a9cc20fd8f2337633ce
pdukaf <pdukaf@outlook.com>
2022-07-17 17:51:26 +0200

    Added logic to initialize a new reservation and added a form to enter data related tto reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,6 +4,7 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @reservation = Reservation.new
   end
 
   def create
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,8 +4,8 @@ class Reservation
   field :hotel_name, type: String
   field :price, type: Float
   field :currency, type: String
-  field :arrival_date, type: Time
-  field :departure_date, type: Time
+  field :arrival_date, type: Date
+  field :departure_date, type: Date
   field :guest_name, type: String
   field :guest_email, type: String
 end
